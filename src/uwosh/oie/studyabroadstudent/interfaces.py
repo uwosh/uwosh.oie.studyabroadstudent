7a9b35fa9906ddceb7939c266ca8f10cca412278
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2017-08-01 14:19:42 -0500

    new content type and vocabularies for OIEStudyAbroadProgram

--- src/uwosh/oie/studyabroadstudent/interfaces.py
+++ src/uwosh/oie/studyabroadstudent/interfaces.py
@@ -9,6 +9,7 @@ from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from plone.supermodel import model
+from plone.namedfile import field
 
 yes_no_none_vocabulary = SimpleVocabulary(
     [
@@ -1566,3 +1567,148 @@ class IOIEStudyAbroadStudentApplication(Interface):
         required=False,
     )
 
+
+class IOIEStudyAbroadProgram(Interface):
+
+    title = schema.TextLine(
+        title=_(u'Program Title'),
+        description=_(u'The full Program Title will be displayed in all OIE print and on-line marketing and in all official OIE program-related materials.  To avoid confusion and increase "brand" awareness for your program, consistently use this program name in full, exactly as it appears here, in your print and electronic media.  Do not include country or city names in this field.'),
+        required=True,
+    )
+
+    description = schema.Text(
+        title=_(u'Description'),
+        description=_(u'This is the description that will be used to promote your program.  Your description should capture the purpose of your program, include an overview of what students will be engaged in while abroad/away, and capture students’ interest! '),
+        required=False,
+    )
+
+    model.fieldset(
+        'comments_fieldset',
+        label=_(u"Comments"),
+        fields=['comments_all', 'comments_oie_leaders', 'comments_oie_all' ]
+    )
+
+    comments_all = schema.Text(
+        title=_(u'Public Comments'),
+        description=_(u'Comments entered here are visible by all system users.'),
+        required=False,
+    )
+
+    comments_oie_leaders = schema.Text(
+        title=_(u'Comments for OIE leadership'),
+        description=_(u'Comments entered here are visible by the OIE Program Manager, Program Liaison, Program Leaders/Co-leaders and site administrators.'),
+        required=False,
+    )
+
+    comments_oie_all = schema.Text(
+        title=_(u'Comments for all OIE users'),
+        description=_(u'Comments entered here are visible by all OIE users.'),
+        required=False,
+    )
+
+    model.fieldset(
+        'program_code_fieldset',
+        label=_(u"Program Code"),
+        fields=['calendar_year', 'term', 'college_or_unit', 'countries', 'program_code',]
+    )
+
+    calendar_year = schema.TextLine(
+        title=_(u'Calendar Year'),
+        description=_(u'Use the year during which the program runs; this is not the year that is associated with the term of study.  For example, a January interim program running from Jan 2-28, 2017 will be associated with "2017".   A program running Dec 28, 2016-Jan 28, 2017 will also be associated with "2017".'),
+        required=True,
+    )
+
+    term = schema.TextLine(
+        title=_(u'Term'),
+        description=_(u''),
+        required=True,
+    )
+
+    college_or_unit = schema.TextLine(
+        title=_(u'College or Unit'),
+        description=_(u''),
+        required=True,
+    )
+
+    countries = schema.Choice(
+        title=_(u'Country or Countries'),
+        description=_(u''),
+        required=True,
+        vocabulary='uwosh.oie.studyabroadstudent.vocabularies.countries',
+    )
+
+    program_code = schema.TextLine(
+        title=_(u'Program Code'),
+        description=_(u'(auto-generated)'),
+        required=True,
+    )
+
+    model.fieldset(
+        'academic_program_fieldset',
+        label=_(u"Academic Program"),
+        fields=[]
+    )
+
+    sponsoring_unit_or_department = schema.TextLine(
+        title=_(u'Sponsoring Unit or Department'),
+        description=_(u'Select all that apply.'),
+        required=True,
+    )
+
+    program_type = schema.Choice(
+        title=_(u'Program Type'),
+        required=True,
+        vocabulary='uwosh.oie.studyabroadstudent.vocabularies.program_type',
+    )
+
+    program_component = schema.Choice(
+        title=_(u'Program Component'),
+        required=True,
+        vocabulary='uwosh.oie.studyabroadstudent.vocabularies.program_component',
+    )
+
+    learning_objectives = schema.Text(
+        title=_(u'Learning Objectives'),
+        description=_(u'State the learning objectives for this program.  Include only one learning objective per text field. These learning objectives will be included in end-of-program assessment and may be used to support Higher Learning Commission and other accreditation processes.'),
+        required=False,
+    )
+
+    equipment_and_space = schema.Choice(
+        title=_(u'Equipment & Space'),
+        description=_(u''),
+        required=True,
+        vocabulary='uwosh.oie.studyabroadstudent.vocabularies.equipment_and_space',
+    )
+
+    equipment_and_space_needs = schema.Text(
+        title=_(u'Equipment & Space details'),
+        description=_(u'if needed'),
+        required=False,
+    )
+
+    guest_lectures = schema.Choice(
+        title=_(u'Guest Lectures'),
+        description=_(u''),
+        required=True,
+        vocabulary='uwosh.oie.studyabroadstudent.vocabularies.guest_lectures',
+    )
+
+
+    initial_draft_program_schedule = field.NamedFile(
+        title=_(u'Initial Draft Program Schedule'),
+        description=_(u'Complete the OIE itinerary form and upload here.'),
+        required=False,
+    )
+
+
+
+
+#"Syllabus & Other Supporting Documents
+#Upload your syllabus plus other related documents (if any).  If you update your syllabus, replace this copy with the updated copy.  This field will remain editable until just prior to travel."
+#"Number of Credits to be Earned by Each Applicant
+#Minimum
+#Maximum"
+#"Language of Study
+#Select all that apply.  Contact the Office of International Education to add a language (abroad@uwosh.edu)."
+#"Cooperating Partners
+#Only entities listed on the UW System Preferred Provider List or academic institutions with a current affiliation agreement with UWO may be selected here.  All other cooperating partners must be selected by following UW System procurement policies."
--- src/uwosh/oie/studyabroadstudent/vocabularies.py
+++ src/uwosh/oie/studyabroadstudent/vocabularies.py
@@ -4,7 +4,7 @@ from plone.i18n.normalizer.interfaces import IIDNormalizer
 from zope.component import queryUtility
 from zope.interface import implementer
 from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 MAX_LENGTH = 250
 
@@ -14,8 +14,8 @@ class SubjectsVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.subjects')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(i, normalizer.normalize(i)) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 SubjectsVocabulary = SubjectsVocabularyFactory()
 
@@ -25,8 +25,8 @@ class MajorsVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.majors')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(i, normalizer.normalize(i)) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 MajorsVocabulary = MajorsVocabularyFactory()
 
@@ -36,8 +36,8 @@ class ProgramsVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.programs')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(normalizer.normalize(i, max_length=MAX_LENGTH), i) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 ProgramsVocabulary = ProgramsVocabularyFactory()
 
@@ -47,8 +47,8 @@ class SessionHoursVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.session_hours')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(normalizer.normalize(i, max_length=MAX_LENGTH), i) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 SessionHoursVocabulary = SessionHoursVocabularyFactory()
 
@@ -58,8 +58,8 @@ class EthnicitiesVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.ethnicities')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(i, normalizer.normalize(i, max_length=MAX_LENGTH)) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 EthnicitiesVocabulary = EthnicitiesVocabularyFactory()
 
@@ -69,8 +69,8 @@ class MarriageStatusesVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.marriage_statuses')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(i, normalizer.normalize(i, max_length=MAX_LENGTH)) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 MarriageStatusesVocabulary = MarriageStatusesVocabularyFactory()
 
@@ -80,8 +80,8 @@ class GendersVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.genders')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(i, normalizer.normalize(i, max_length=MAX_LENGTH)) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 GendersVocabulary = GendersVocabularyFactory()
 
@@ -91,8 +91,8 @@ class StatesForResidencyVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.states_for_residency')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(i, normalizer.normalize(i, max_length=MAX_LENGTH)) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 StatesForResidencyVocabulary = StatesForResidencyVocabularyFactory()
 
@@ -102,7 +102,64 @@ class CitizenshipVocabularyFactory(object):
     def __call__(self, context):
         values = api.portal.get_registry_record('oiestudyabroadstudent.citizenship')
         normalizer = queryUtility(IIDNormalizer)
-        items = [(i, normalizer.normalize(i, max_length=MAX_LENGTH)) for i in values]
-        return SimpleVocabulary.fromItems(items)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
 
 CitizenshipVocabulary = CitizenshipVocabularyFactory()
+
+@implementer(IVocabularyFactory)
+class CountriesVocabularyFactory(object):
+
+    def __call__(self, context):
+        values = api.portal.get_registry_record('oiestudyabroadstudent.countries')
+        normalizer = queryUtility(IIDNormalizer)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
+
+CountriesVocabulary = CountriesVocabularyFactory()
+
+@implementer(IVocabularyFactory)
+class ProgramTypeVocabularyFactory(object):
+
+    def __call__(self, context):
+        values = api.portal.get_registry_record('oiestudyabroadstudent.program_type')
+        normalizer = queryUtility(IIDNormalizer)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
+
+ProgramTypeVocabulary = ProgramTypeVocabularyFactory()
+
+@implementer(IVocabularyFactory)
+class ProgramComponentVocabularyFactory(object):
+
+    def __call__(self, context):
+        values = api.portal.get_registry_record('oiestudyabroadstudent.program_component')
+        normalizer = queryUtility(IIDNormalizer)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
+
+ProgramComponentVocabulary = ProgramComponentVocabularyFactory()
+
+@implementer(IVocabularyFactory)
+class EquipmentAndSpaceVocabularyFactory(object):
+
+    def __call__(self, context):
+        values = api.portal.get_registry_record('oiestudyabroadstudent.equipment_and_space')
+        normalizer = queryUtility(IIDNormalizer)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
+
+EquipmentAndSpaceVocabulary = EquipmentAndSpaceVocabularyFactory()
+
+@implementer(IVocabularyFactory)
+class GuestLecturesVocabularyFactory(object):
+
+    def __call__(self, context):
+        values = api.portal.get_registry_record('oiestudyabroadstudent.guest_lectures')
+        normalizer = queryUtility(IIDNormalizer)
+        items = [SimpleTerm(value=i, title=i) for i in values]
+        return SimpleVocabulary(items)
+
+GuestLecturesVocabulary = GuestLecturesVocabularyFactory()
+
+
