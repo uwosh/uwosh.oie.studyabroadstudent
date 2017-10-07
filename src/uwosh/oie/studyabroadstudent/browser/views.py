b3445086e2de9a414b9726fab5dcf8f65818ca1e
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2017-10-07 00:01:25 -0500

    add custom program view to render rich text description correctly

--- src/uwosh/oie/studyabroadstudent/browser/views.py
+++ src/uwosh/oie/studyabroadstudent/browser/views.py
@@ -1,24 +1,10 @@
 from Acquisition import aq_inner
 from uwosh.oie.studyabroadstudent.interfaces import IOIEStudyAbroadStudentApplication
 from plone import api
-# from Products.Five import BrowserView
-
-
-# class ApplicationView(BrowserView):
-
-#     def sessions(self):
-#         """Return a catalog search result of sessions to show."""
-
-#         context = aq_inner(self.context)
-#         catalog = api.portal.get_tool(name='portal_catalog')
-
-#         return catalog(
-#             object_provides=ISession.__identifier__,
-#             path='/'.join(context.getPhysicalPath()),
-#             sort_on='sortable_title')
-
 from plone.dexterity.browser.view import DefaultView
 
 class ApplicationView(DefaultView):
+    pass
 
+class ProgramView(DefaultView):
     pass
