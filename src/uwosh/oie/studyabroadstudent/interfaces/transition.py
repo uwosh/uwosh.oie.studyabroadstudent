fd0db3aa1d350ab1b65332ec52100af2d89f0b7a
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2018-05-22 14:16:16 -0500

    add dexteritytextindexer

--- src/uwosh/oie/studyabroadstudent/interfaces/transition.py
+++ src/uwosh/oie/studyabroadstudent/interfaces/transition.py
@@ -5,6 +5,7 @@ from zope import schema
 from zope.interface import Interface
 from uwosh.oie.studyabroadstudent.vocabularies import RegistryValueVocabulary
 from plone.directives import form
+from collective import dexteritytextindexer
 
 
 class IOIETransition(Interface):
@@ -14,16 +15,24 @@ class IOIETransition(Interface):
         required=False,
         default=_(u'will be auto-generated on save'),
     )
+
     transitionDate = schema.Date(title=_(u'Transition Date'))
+
+    dexteritytextindexer.searchable('destinationCity')
     destinationCity = schema.TextLine(title=_(u'Destination City'))
+
+    dexteritytextindexer.searchable('destinationCountry')
     destinationCountry = schema.Choice(title=_(u'Destination Country'),
                                        source=RegistryValueVocabulary('oiestudyabroadstudent.countries'))
+
     accommodation = schema.Choice(title=_(u'Accommodation'),
                                   source=RegistryValueVocabulary('oiestudyabroadstudent.accommodation'))
+
     accommodationRoomSizes = schema.List(
         title=_(u'Room Size(s)'),
         value_type=schema.Choice(source=RegistryValueVocabulary('oiestudyabroadstudent.room_size'))
     )
+
     transitionType = schema.Choice(
         title=_(u'Transition Type'),
         source=RegistryValueVocabulary('oiestudyabroadstudent.transition_type'),
