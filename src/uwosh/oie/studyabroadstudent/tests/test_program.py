a5a14ab1a457027c63a3eeb6c221a94bc23f1ace
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2018-07-17 13:09:05 -0500

    add start of tests for OIEStudyAbroadProgram type

---
+++ src/uwosh/oie/studyabroadstudent/tests/test_program.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.dexterity.interfaces import IDexterityFTI
+from uwosh.oie.studyabroadstudent.interfaces.studyabroadprogram import IOIEStudyAbroadProgram
+from uwosh.oie.studyabroadstudent.interfaces.calendaryear import IOIECalendarYear
+from uwosh.oie.studyabroadstudent.testing import UWOSH_OIE_STUDYABROADSTUDENT_INTEGRATION_TESTING  # noqa
+from zope.component import createObject
+from zope.component import queryUtility
+
+import unittest
+
+
+class OIEStudyAbroadProgramIntegrationTest(unittest.TestCase):
+
+    layer = UWOSH_OIE_STUDYABROADSTUDENT_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.installer = api.portal.get_tool('portal_quickinstaller')
+        # add calendar year
+        calendar_year = api.content.create(
+            container=self.portal,
+            type='OIECalendarYear',
+            id='2018'
+        )
+        self.calendar_year_uid = api.content.get_uuid(obj=calendar_year)
+
+    def test_schema(self):
+        fti = queryUtility(IDexterityFTI, name='OIEStudyAbroadProgram')
+        schema = fti.lookupSchema()
+        self.assertEqual(IOIEStudyAbroadProgram, schema)
+
+    def test_fti(self):
+        fti = queryUtility(IDexterityFTI, name='OIEStudyAbroadProgram')
+        self.assertTrue(fti)
+
+    def test_factory(self):
+        fti = queryUtility(IDexterityFTI, name='OIEStudyAbroadProgram')
+        factory = fti.factory
+        obj = createObject(factory)
+        self.assertTrue(IOIEStudyAbroadProgram.providedBy(obj))
+
+    def test_adding(self):
+        obj = api.content.create(
+            container=self.portal,
+            type='OIEStudyAbroadProgram',
+            id='OIEStudyAbroadProgram',
+            calendar_year=self.calendar_year_uid,
+            term='1 Fall Interim',
+            college_or_unit='B College of Business',
+            countries=['Afghanistan'],
+        )
+        self.assertTrue(IOIEStudyAbroadProgram.providedBy(obj))
