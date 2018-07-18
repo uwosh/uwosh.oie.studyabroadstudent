6fda1570866715529e47f6c06dd0f00345645497
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2018-07-18 16:54:38 -0500

    add tests for adding a program, checking that add-on is installed, transitions that exist or do not exist, whether anonymous can transition or edit, correct workflow is assigned

--- src/uwosh/oie/studyabroadstudent/tests/test_program.py
+++ src/uwosh/oie/studyabroadstudent/tests/test_program.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+from AccessControl import Unauthorized
 from plone import api
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
+from plone.api.exc import InvalidParameterError
+from plone.app.testing import setRoles, TEST_USER_ID, TEST_USER_NAME, login, logout
 from plone.dexterity.interfaces import IDexterityFTI
+from Products.CMFCore.utils import getToolByName
 from uwosh.oie.studyabroadstudent.interfaces.studyabroadprogram import IOIEStudyAbroadProgram
-from uwosh.oie.studyabroadstudent.interfaces.calendaryear import IOIECalendarYear
 from uwosh.oie.studyabroadstudent.testing import UWOSH_OIE_STUDYABROADSTUDENT_INTEGRATION_TESTING  # noqa
 from zope.component import createObject
 from zope.component import queryUtility
@@ -21,13 +22,28 @@ class OIEStudyAbroadProgramIntegrationTest(unittest.TestCase):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.installer = api.portal.get_tool('portal_quickinstaller')
+
         # add calendar year
-        calendar_year = api.content.create(
+        self.calendar_year = api.content.create(
             container=self.portal,
             type='OIECalendarYear',
             id='2018'
         )
-        self.calendar_year_uid = api.content.get_uuid(obj=calendar_year)
+        self.calendar_year_uid = api.content.get_uuid(obj=self.calendar_year)
+
+        # add a sample program
+        self.program = api.content.create(
+            container=self.portal,
+            type='OIEStudyAbroadProgram',
+            id='sample-program',
+            calendar_year=self.calendar_year_uid,
+            term='1 Fall Interim',
+            college_or_unit='B College of Business',
+            countries=['Afghanistan'],
+        )
+
+    def test_is_addon_installed(self):
+        self.assertTrue(self.installer.isProductInstalled('uwosh.oie.studyabroadstudent'))
 
     def test_schema(self):
         fti = queryUtility(IDexterityFTI, name='OIEStudyAbroadProgram')
@@ -44,14 +60,49 @@ class OIEStudyAbroadProgramIntegrationTest(unittest.TestCase):
         obj = createObject(factory)
         self.assertTrue(IOIEStudyAbroadProgram.providedBy(obj))
 
-    def test_adding(self):
+    def test_can_add_a_program(self):
         obj = api.content.create(
             container=self.portal,
             type='OIEStudyAbroadProgram',
-            id='OIEStudyAbroadProgram',
+            id='sample-program-test_adding',
             calendar_year=self.calendar_year_uid,
             term='1 Fall Interim',
             college_or_unit='B College of Business',
             countries=['Afghanistan'],
         )
         self.assertTrue(IOIEStudyAbroadProgram.providedBy(obj))
+
+    def test_can_transition_by_manager(self):
+        obj = self.program
+        api.content.transition(obj=obj, transition='submit-to-chair')
+        state = api.content.get_state(obj=obj)
+        self.assertEqual(state, 'pending-chair-review')
+
+    def test_nonexistent_transition_by_manager(self):
+        obj = self.program
+        error_str = ''
+        try:
+            api.content.transition(obj=obj, transition='this-transition-does-not-exist')
+        except InvalidParameterError as e:
+            error_str = e.message
+        self.assertTrue('Invalid transition' in error_str)
+
+    def test_cannot_transition_as_anonymous(self):
+        portal = self.layer['portal']
+        login(portal, TEST_USER_NAME)
+        obj = self.program
+        logout()
+        self.assertRaises(InvalidParameterError, api.content.transition, obj=obj, transition='submit-to-chair')
+
+    def test_not_editable_by_anonymous(self):
+        portal = self.layer['portal']
+        logout()
+        self.assertRaises(Unauthorized, portal.restrictedTraverse, 'sample-program/@@edit')
+
+    def test_correct_default_workflow(self):
+        portal = self.layer['portal']
+        workflowTool = getToolByName(portal, 'portal_workflow')
+        chains = dict(workflowTool.listChainOverrides())
+        defaultChain = workflowTool.getDefaultChain()
+        programChain = chains.get('OIEStudyAbroadProgram', defaultChain)
+        self.assertEqual(programChain, ('programmanagement',))
