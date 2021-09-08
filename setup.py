c005e2d1fc0cf51be1cf564976b6ff67cc311c27
Brian Duncan <brian.duncan@wildcardcorp.com>
2021-09-08 16:51:03 -0500

    Back to development: 1.0.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='uwosh.oie.studyabroadstudent',
-    version='1.0.5',
+    version='1.0.6.dev0',
     description='Dexterity content type for study abroad programs and student '
                 'applications for the University of Wisconsin Oshkosh Office '
                 'of International Education',
